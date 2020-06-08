7550f40e94e19b43be3bb384017ec86f3495f57e
Shounak5775 <66526114+Shounak5775@users.noreply.github.com>
2020-06-08 20:51:41 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,14 @@
+list1 = [12,-7,5,64,-14]
+for i in list1:
+    if i < 0:
+        continue
+    else:
+        print (i)
+        
+        
+list2 = [12,14,-95,3]
+for j in list2:
+    if j < 0:
+        continue
+    else:
+        print (j)
